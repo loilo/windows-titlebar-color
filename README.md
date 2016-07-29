5ecb65ea138f15c34f2e027673f35a4319afbf38
Florian Reuschel <florian.reuschel@gmx.de>
2016-07-30 01:35:04 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# windows-titlebar-color
+Reads the title bar's color from the Windows 7, 8, 8.1 or 10 registry.
+
+Created for usage in [Electron](https://github.com/electron/electron) apps.
+
+[![JavaScript Style Guide](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+
+## Usage
+```
+const winColor = require('windows-titlebar-color')
+
+console.log(winColor.titlebarColor)
+
+// could be #4ca0fe. or anything else.
+//           ^ this is at least not a completely arbitrary color since it's the default blue window chrome color that ships with Windows 8
+```
+
+## Generally Works with...
+
+...Windows 7, Windows 8/8.1 and Windows 10. Generally.
+
+
+## Does not work with...
+
+...Windows 7 "Basic" style (non-Aero), classic window style (as in Win2k and below), high contrast window style and any other kind of manually tweaked window styles.
+
+If you know how to elegantly provide information for those, feel free to open an issue. I'm generally fine with the lack of those since that's where other software (such as Google Chrome) also draws the line to fall back to their own non-OS-following styles.
+
+Also note that the color determined for Windows 7 in Aero mode may differ from the perceived color of the title bar due to glossy transparency magic.
+
+## API
+
+It's pretty simple. Grab the module and then go for it:
+
+`const winColor = require('windows-titlebar-color')`
+
+### Stuff you can access on the `winColor` object:
+
+#### `winColor.titlebarColor`
+Returns the color of the your Windows machines' title bar as a hex string.
+
+#### `winColor.inactiveTitlebarColor`
+Returns the color of the title bar on an inactive window.
+
+#### `winColor.isSupported`
+Returns a boolean indicating if the running OS is generally supported to read colors from. Does not check for other limitations like those stated above.
+
+#### `winColor.reload()`
+Freshly loads the colors from the Registry. This is also done on initiation so typically you won't ever need to call this.
+
+#### `winColor.raw`
+Returns a hash containing the registry values from the `HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\DWM` key entries, converted to either boolean or hex color strings. For example my personal Windows 10 hash looks like this:
+
+```
+{
+  Composition: true,
+  ColorizationGlassAttribute: false,
+  EnableAeroPeek: true,
+  ColorPrevalence: true,
+  AccentColor: '#484a4c',
+  ColorizationColor: '#4c4a48',
+  ColorizationColorBalance: '#000059',
+  ColorizationAfterglow: '#4c4a48',
+  ColorizationAfterglowBalance: '#00000a',
+  ColorizationBlurBalance: true,
+  EnableWindowColorization: true
+}
+  ```
---
+++ WindowsAccentColors.js
@@ -0,0 +1,118 @@
+/**
+ * This class allows reading the Windows accent colors from the registry.
+ * Works in Windows 10 and above (7 and 8 not tested yet)
+ */
+class WindowsAccentColors {
+  constructor () {
+    this.reload()
+    this.checkSupport()
+  }
+
+  /**
+   * Checks for the OS and release and sets corresponding variables
+   * @return {void}
+   */
+  checkSupport () {
+    const os = require('os')
+    this.isWin = os.platform() === 'win32'
+    this.isWin10 = this.isWin && parseInt(os.release()) === 10
+    this.isWin8 = this.isWin && [6.2, 6.3].indexOf(parseFloat(os.release())) !== -1
+    this.isWin7 = this.isWin && parseFloat(os.release()) === 6.1
+
+    this.isSupported = this.isWin7 || this.isWin8 || this.isWin10
+  }
+
+  /**
+   * Rewrites a registry hex value to a hex color string. Ignores alpha channel.
+   * @param  {string} hex The hex value from the registry
+   * @return {string} A hex color string in #rrggbb format
+   */
+  hex2color (hex) {
+    return '#' +
+      '0'.repeat(Math.max(0, 8 - hex.length)) +
+      hex.substr(2 + Math.max(0, hex.length - 8))
+  }
+
+  /**
+   * Parses a registry hex value to a boolean.
+   * @param  {string} hex The hex value from the registry
+   * @return {boolean} The converted boolean
+   */
+  hex2bool (hex) {
+    return hex !== '0x0'
+  }
+
+  /**
+   * Reloads the accent color configuration from the registry
+   * @return {void}
+   */
+  reload () {
+    const os = require('os')
+    this.accentData = require('child_process')
+      .execSync('REG QUERY HKCU\\SOFTWARE\\Microsoft\\Windows\\DWM')
+      .toString('utf-8')
+      .trim()
+      .split(os.EOL)
+      .slice(1)
+      .reduce((carry, row) => {
+        const rowData = row.trim().split('    ')
+        return Object.assign({}, carry, {
+          [rowData[0]]: rowData[2]
+        })
+      }, {})
+
+    // The keys that should be handled as booleans
+    const booleans = [
+      'Composition',
+      'ColorizationBlurBalance',
+      'ColorizationGlassAttribute',
+      'EnableAeroPeek',
+      'ColorPrevalence',
+      'EnableWindowColorization'
+    ]
+
+    // We'll just handle everything unknown as a color
+    Object.keys(this.accentData).forEach(key => {
+      if (booleans.indexOf(key) !== -1) {
+        this.accentData[key] = this.hex2bool(this.accentData[key])
+      } else {
+        this.accentData[key] = this.hex2color(this.accentData[key])
+      }
+    })
+  }
+
+  /**
+   * Gets the parsed registry items
+   * @return {Object} An object with the item names as keys and hex color strings/booleans as values
+   */
+  get raw () {
+    return this.accentData
+  }
+
+  /**
+   * Gets the titlebar's color
+   * @return {string} The hex code of the title bar color
+   */
+  get titlebarColor () {
+    if (this.isWin10) {
+      return this.raw.ColorPrevalence == null ? this.raw.ColorizationColor : '#ffffff'
+    } else {
+      return this.raw.ColorizationColor
+    }
+  }
+
+  /**
+   * Gets the inactive titlebar's color
+   * @return {string} The hex code of the title bar color when it's inactive
+   */
+  get inactiveTitlebarColor () {
+    if (this.isWin10) {
+      // Inactive window color is interchangeable in Win10 by addinc the 'AccentColorInactive' entry
+      return this.raw.AccentColorInactive != null ? this.raw.AccentColorInactive : '#ffffff'
+    } else {
+      return '#ebebeb'
+    }
+  }
+}
+
+module.exports = new WindowsAccentColors()
